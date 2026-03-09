6104e7b4d17eb32fcae6fc34dde5eb738df81a4c
GABRYEL FILLIPE CAVALCANTI DA SILVA <25132404@dsn127m1241955.cfp127aluno.local>
2026-03-09 07:59:53 -0300

    Corrigindo tipo do State para tipo IntState para melhor adequação as boas praticas

--- app/src/main/java/com/example/exerciciocontadoridade/MainActivity.kt
+++ app/src/main/java/com/example/exerciciocontadoridade/MainActivity.kt
@@ -10,34 +10,28 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.exerciciocontadoridade.ui.theme.ExercicioContadorIdadeTheme
-import kotlin.toString
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,31 +47,9 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-@Composable
-fun InfoInicial(modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        Text(
-            text = "Qual é a sua Idade?",
-            textAlign = TextAlign.Center,
-            color = Color(0, 174, 255, 244),
-            modifier = Modifier.fillMaxWidth(),
-            fontSize = 24.sp,
-            fontFamily = FontFamily.Monospace
-        )
-        Text(
-            text = "Aperte os botões para informar sua idade",
-            modifier = Modifier.fillMaxWidth(),
-            textAlign = TextAlign.Center,
-        )
-    }
-}
-
 @Composable
 fun ContadorScreen(modifier: Modifier = Modifier) {
-    var idade by remember { mutableStateOf(0) }
+    var idade by remember { mutableIntStateOf(0) }
 
     val status = if (idade >= 18) "Maior" else "Menor"
 
